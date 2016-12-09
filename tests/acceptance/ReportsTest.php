77620a4147d083b760a71855bb23edc83ee9301a
Rafael <rafaelpviegas@gmail.com>
2016-12-09 03:07:13 -0200

    Testes

--- tests/acceptance/ReportsTest.php
+++ tests/acceptance/ReportsTest.php
@@ -7,13 +7,13 @@ use Tests\AcceptanceTestCase;
 class ReportsTest extends AcceptanceTestCase
 {
 
-    public function testAbout()
+    public function testAlertsVehicles()
     {
         $this->get('/reports/alerts/vehicles');
         $this->assertEquals($this->response->status(), 200);
     }
     
-    public function testAbout()
+    public function testHistoryVehicles()
     {
         $this->get('/reports/history/vehicles');
         $this->assertEquals($this->response->status(), 200);
